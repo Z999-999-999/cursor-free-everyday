3f7c185f2de52126220d0d1eb88ef5815abda173
agentcodee <1344553400@qq.com>
2025-07-14 11:57:45 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,33 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#怕很多小白不懂,所以用py写了这个
-"""
-全网独家技术,请勿用于商业目的,仅供技术研究交流学习
-cursor注册机,邮箱验证码接收工具
-
-使用说明:
-首先到达cursor接收邮箱验证码部分
-1. 修改下面的地址再运行: python example.py
-   将使用默认邮箱地址获取验证码
-
-2. 指定邮箱: python example.py 网站 lordsem89@storetaikhoan.com
-   将使用指定的邮箱地址获取验证码
-
-注意事项:
-- 邮箱前缀可以自定义，域名部分请使用提供的域名列表中的域名
-- 注册网站时先使用此邮箱，然后用此工具接收验证码
-- 验证码获取可能需要等待几秒钟
-
-✓ QQ群883248964,Github开源地址https://github.com/agentcodee/cursor-free-everyday
-
-运行结果预览:
-邮箱地址: lordsem89@storetaikhoan.com
-开始获取邮箱 lordsem89@storetaikhoan.com 的验证码...
-尝试 1/3: 请求验证码...
-成功获取验证码: 938298
-"""
-
 import sys
 import time
 import requests
@@ -99,7 +72,7 @@ def check_server_health(server_url):
 def main():
     """主函数"""
     # 设置默认值 这个不变
-    server_url = "http://14.103.190.198:5362"
+    server_url = "http://更换自己搭建的"
     # server_url = "http://127.0.0.1:5362"
 
     # 在项目中的txt提供了大量的域名,从里面随便选一个
